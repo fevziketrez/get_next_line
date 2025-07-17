c1d86af493866a071043f7f489bac0de184303cb
Fevzi Ketrez <fevzi.ketrez@gmail.com>
2025-07-17 20:59:41 +0300

    vers1. test

--- get_next_line.c
+++ get_next_line.c
@@ -5,9 +5,25 @@ fd = open(fd, O_RDONLY, 0644)
 
 char *get_next_line(int fd)
 {
-    
-    buffer = read(fd, O_RDONLY, BUF_SIZE);
-    
 
+    read_size = read(buffer, O_RDONLY, BUF_SIZE);
+
+	int i;
+
+	i = 0;
+	while(buffer[i] && buffer[i] != '\n')
+		i++;
+	// maybe if buffer[i] == \n ?? to return		AAA
+	res = XXsubstr(buffer, 0, i);
+	buffer = XXsubstr(buffer, i, ft_strlen(buffer));
+	if (strchr(res, '\n')) ///						AAA SAME WITH UP
+		return res
+	else if (read_size < BUF_SIZE && !strchr(buffer, '\n') && strlen(buffer) == 0)
+	{
+		//BREAK END OF FILE
+	}
+
+	else if (i == read_size)
+		//read again ++
 
 }
