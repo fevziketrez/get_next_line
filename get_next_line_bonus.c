1987fc0a000237ee55fccb4f395569e0ed2e727a
Fevzi Ketrez <fevzi.ketrez@gmail.com>
2025-08-02 12:36:52 +0300

    memcpy reafactored into substr, calloc implemented as ft_calloc

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -1,30 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fketrez <fketrez@student.42istanbul.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/24 03:28:41 by fketrez           #+#    #+#             */
-/*   Updated: 2025/08/02 12:21:01 by fketrez          ###   ########.fr       */
+/*   Updated: 2025/08/02 12:24:51 by fketrez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static void	gnl_helper(char **res, char **left)
+static void	gnl_helper(char **res, char **left_fd)
 {
 	if (strchr_i((*res), 0, '\n') != -1)
 	{
-		free((*left));
-		(*left) = ft_substr((*res), strchr_i((*res), 0, '\n') + 1,
+		free(*(left_fd));
+		(*left_fd) = ft_substr((*res), strchr_i((*res), 0, '\n') + 1,
 				ft_strlen((*res)));
 		(*res)[strchr_i((*res), 0, '\n') + 1] = '\0';
 	}
 	else
 	{
-		free((*left));
-		(*left) = NULL;
+		free(*(left_fd));
+		(*left_fd) = NULL;
 	}
 	if ((*res) && (*res)[0] == '\0')
 	{
@@ -39,22 +39,22 @@ char	*get_next_line(int fd)
 	ssize_t		read_size;
 	char		*res;
 	char		*temp;
-	static char	*left;
+	static char	*left[1024];
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	res = left;
+	res = left[fd];
 	read_buf = ft_calloc(BUFFER_SIZE + 1, 1); //calloc
 	read_size = 1;
 	while (strchr_i(read_buf, 0, '\n') == -1 && read_size > 0)
 	{
 		read_size = read(fd, read_buf, BUFFER_SIZE);
 		temp = ft_strjoin(res, read_buf, read_size);
-		if (res != left)
+		if (res != left[fd])
 			free(res);
 		res = temp;
 	}
 	free(read_buf);
-	gnl_helper(&res, &left);
+	gnl_helper(&res, &(left[fd]));
 	return (res);
 }
--- main.c
+++ main.c
@@ -6,23 +6,126 @@
 /*   By: fketrez <fketrez@student.42istanbul.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/24 03:37:30 by fketrez           #+#    #+#             */
-/*   Updated: 2025/07/24 03:37:46 by fketrez          ###   ########.fr       */
+/*   Updated: 2025/08/02 12:32:27 by fketrez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
 
-int	main(void)
-{
-	ssize_t	fd;
-	char	*line;
+// void	printlinesbro(int fd)
+// {
+// 	char *line= get_next_line(fd);
+// 	
+// // 	printlinesbro(fd);
+// // 	printf("%s", line);
+// // 	free(line);
+// // }
+// int	main(void)
+// {
+// 	ssize_t	fd;
+// 	// char	*line;
 
-	fd = open("test.txt", O_RDONLY, 0777);
-	while (line)
+// 	fd = open("test.txt", O_RDONLY);
+// 	line = get_next_line(fd);
+// 	while (line)
+// 	{
+// 		printf("%s", line);
+// 		free(line);
+// 		line = get_next_line(fd);
+// 	}
+// 	// printf("%s", line);
+
+
+// // 	// char *lines[1024];
+// // 	// char *line;
+// // 	// int i = 0;
+// // 	fd = open("test.txt", O_RDONLY);
+// // 	// line = get_next_line(fd);
+// // 	// while (line)
+// // 	// {
+// // 	// 	lines[i] = line;
+// // 	// 	line = get_next_line(fd);
+// // 	// 	i++;
+// // 	// }
+// // 	// i--;
+// // 	// while(i >= 0)
+// // 	// {
+// // 	// 		printf("%s", lines[i]);
+// // 	// 		free(lines[i]);
+// // 	// 		i--;
+// // 	// }
+// // 	// char *lines[1024];
+// // 	// i = 0;
+// // 	char *terminal;
+// // 	terminal = get_next_line(0);
+// // 	while(terminal)
+// // 	{
+// // 		printf("%s", terminal);
+// // 		free(terminal);
+// // 		terminal = get_next_line(0);
+// // 	}
+
+// }
+
+// void print_file(char *filename)
+// {
+// 	ssize_t fd;
+// 	char *line;
+// 	fd = open(filename, O_RDONLY);
+// 	line = get_next_line(fd);
+// 	while (line)
+// 	{
+// 		printf("%s", line);
+// 		free(line);
+// 		line = get_next_line(fd);
+// 	}
+// }
+
+// int main(int argc, char **argv)
+// {
+// 	if (argc == 1)
+// 		return 0;
+// 	int i = 1;
+
+// 	while(argv[i])
+// 	{
+// 		print_file(argv[i]);
+// 		i++;
+// 	}
+// 	return 0;
+// }
+
+
+int main()
+{
+	int fd1 = open("test1.txt", O_RDONLY);
+	int fd2 = open("test2.txt", O_RDONLY);
+	char *test = malloc(100);
+	char *line1;
+	char *line2;
+	// test[0] = '\t';
+	line1 = test;
+	line2 = test;
+	while (line1 || line2)
 	{
-		line = get_next_line(fd);
-		printf("%s", line);
-		free(line);
+		if (line1)
+		{
+			line1 = get_next_line(fd1);
+			printf("%s", line1);
+			free(line1);
+		}
+		if (line2)
+		{
+
+			line2 = get_next_line(fd2);
+			printf("%s", line2);
+			free(line2);
+		}
+
 	}
+	// if (*test == '\t')
+	free(test);
+	// free(line1);
+	// free(line2);
 }
